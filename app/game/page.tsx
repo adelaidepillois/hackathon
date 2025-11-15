a8760494e8474091436c5d12b3568a8b9eecd89d
Gaya-Khelili <kheliligaya@gmail.com>
2025-11-15 14:35:56 +0100

    Add custom gradient background and full-height styling to game page

---
+++ app/game/page.tsx
@@ -0,0 +1,8 @@
+import styles from './game.module.css'
+
+export default async function Page() {
+    
+    return (
+        <div className={`${styles['bg-custom-gradient']} ${styles['min-h-screen']}`}>test</div>
+    )
+}
