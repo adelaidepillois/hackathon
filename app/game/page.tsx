4737d3df42319329a53ba3b639c81a41101bc5cb
Gaya-Khelili <kheliligaya@gmail.com>
2025-11-15 15:12:29 +0100

    Display username on game page with styled container

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,8 +1,13 @@
 import styles from './game.module.css'
+import '../globals.css'
 
 export default async function Page() {
-    
+    const username = "Username";
     return (
-        <div className={`${styles['bg-custom-gradient']} ${styles['min-h-screen']}`}>test</div>
+        <div className={`${styles['bg-custom-gradient']} ${styles['min-h-screen']}`}>
+            <div className="absolute left-[86px] top-[40px] w-[172px] h-[40px] bg-white text-[#2162DD] opacity-100 rounded-[1000px] flex items-center justify-center shadow font-neue">
+                {username}
+            </div>
+        </div>
     )
 }
