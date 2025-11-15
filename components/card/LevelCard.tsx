02c5c82e449a45c05314e1fe20f331a7e5c5da2b
Adélaïde <74415100+adelaidepillois@users.noreply.github.com>
2025-11-15 20:04:15 +0100

    Add hover scale effect to level cards

--- components/card/LevelCard.tsx
+++ components/card/LevelCard.tsx
@@ -12,7 +12,7 @@ export default function LevelCard({
 	className,
 }: LevelCardProps) {
 	return (
-		<div className={`flex flex-col justify-between px-5 py-4 border-white border rounded-[10px] bg-[hsl(219,73%,50%,0.3)] backdrop-blur-md text-white flex flex-col w-full lg:max-w-sm lg:w-[22rem]
+		<div className={`flex flex-col justify-between px-5 py-4 border-white border rounded-[10px] bg-[hsl(219,73%,50%,0.3)] backdrop-blur-md text-white flex flex-col w-full lg:max-w-sm lg:w-[22rem] transition-transform duration-300 hover:scale-105
         ${className ?? ""}
       `}>
 			<h2 className={styles.levelCardTitle}>
