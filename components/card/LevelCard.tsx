ac9a2becb825789e97e1a22df658e23fb3abe19a
Adélaïde <74415100+adelaidepillois@users.noreply.github.com>
2025-11-15 23:08:08 +0100

    fix: nettoyage du code dupliqué et correction du déblocage des niveaux

--- components/card/LevelCard.tsx
+++ components/card/LevelCard.tsx
@@ -27,18 +27,21 @@ export default function LevelCard({
 			<h2 className={styles.levelCardTitle}>{title}</h2>
 			<div>
 				<p className={styles.levelCardDescription}>{description}</p>
-				<p className="mt-4 font-bold text-lg">Biais introduits:</p>
-				<ul>
-					{biases &&
-						biases.map((bias, index) => (
-							<li
-								key={index}
-								className="text-md mt-1 list-disc list-outside ml-5"
-							>
-								{bias}
-							</li>
-						))}
-				</ul>
+				{biases && biases.length > 0 && (
+					<>
+						<p className="mt-4 font-bold text-lg">Biais introduits:</p>
+						<ul>
+							{biases.map((bias, index) => (
+								<li
+									key={index}
+									className="text-md mt-1 list-disc list-outside ml-5"
+								>
+									{bias}
+								</li>
+							))}
+						</ul>
+					</>
+				)}
 			</div>
 			<div className="flex justify-between items-center mt-6">
 				<p className={styles.levelCardAction}>Commencer</p>
